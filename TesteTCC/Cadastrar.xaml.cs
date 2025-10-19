ada3d974444f027d641d6c90b867cda0ce92f44b
evellynfm <evellynfrancyne7@gmail.com>
2025-10-19 12:04:51 -0300

    Teste

--- TesteTCC/Cadastrar.xaml.cs
+++ TesteTCC/Cadastrar.xaml.cs
@@ -6,4 +6,10 @@ public partial class Cadastrar : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        // Voltar para a página anterior
+        await Navigation.PopAsync();
+    }
 }
